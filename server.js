30f6a1dba9c9cc06837b20eec3dbbfa1675b03dc
Franyer Verjel <franyer.verjel@gmail.com>
2020-05-01 13:51:27 -0500

    Listo para el deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
